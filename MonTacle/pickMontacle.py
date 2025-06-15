f879e10d214b50801260aab4438c63fa0973e982
pineaxolotl <mathmaster10@outlook.com>
2025-06-15 12:29:07 +0200

    Update pickMontacle.py

--- MonTacle/pickMontacle.py
+++ MonTacle/pickMontacle.py
@@ -1,11 +1,13 @@
 import random
+
+cards_dir = os.path.join(os.path.dirname(__file__), ".."))
 cards = []
 
-for path in os.listdir(r"MonTac-Cards\\"):
+for path in os.listdir(cards_dir):
   if path.endswith(".PNG"):
     cards.append(path)
 
 n = random.randrange(0, len(cards)-1)
 montaclePick = cards[n]
-with open('Montac-Cards\\MonTacle\\montacle.txt', 'w') as f:
+with open('montacle.txt', 'w') as f:
   f.write(montaclePick)
