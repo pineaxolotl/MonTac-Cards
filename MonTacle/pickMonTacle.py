7376975e7b09226fc0c14495b6e46f1d8cd2848e
pineaxolotl <mathmaster10@outlook.com>
2025-06-15 14:00:16 +0200

    Update pickMonTacle.py

--- MonTacle/pickMonTacle.py
+++ MonTacle/pickMonTacle.py
@@ -6,7 +6,7 @@ cards = []
 
 blacklist_path = os.path.join(os.path.dirname(__file__), "blacklist.txt")
 with open(blacklist_path, 'r') as f:
-  blacklist = [line.strip() + ".PNG" for line in f]
+  blacklist = [line.strip() for line in f]
 
 for path in os.listdir(cards_dir):
   if path.endswith(".PNG") and path not in blacklist:
@@ -17,10 +17,9 @@ montaclePick = cards[n].replace(".PNG", "")
 
 output_path = os.path.join(os.path.dirname(__file__), "montacle.txt")
 with open(output_path, 'w') as f:
-  f.write(montaclePick)
+  f.write(montaclePick + ".PNG")
 
 with open(blacklist_path, 'w') as f:
-  lines = f.readlines()
   if len(blacklist) >= 30:
-    f.writelines(lines[1:])
+    f.writelines(blacklist[1:])
   f.write(montaclePick)
